0456320a528e905b5dcb8b2a925a7bb0d23b178c
masterpaulo <dev.masterpaulo@gmail.com>
2022-06-06 20:38:21 +0800

    Add share functionality

--- photo-list/Views/Details View/DetailsViewController.swift
+++ photo-list/Views/Details View/DetailsViewController.swift
@@ -23,6 +23,7 @@ class DetailsViewController: UIViewController {
         super.viewDidLoad()
         
         setup()
+        setupShareNavButton()
     }
     // MARK: - Setup
     
@@ -30,6 +31,23 @@ class DetailsViewController: UIViewController {
         title = vm?.idLabelText ?? "no data found"
         imageView.loadImage(fromURL: vm?.photoUrl ?? "")
         titleLabel.text = vm?.titleText ?? ""
+        
+    }
+    
+    private func setupShareNavButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTextButton(_:)))
+    }
+    
+    // MARK: - Actions
+    @objc func shareTextButton(_ sender: Any) {
+        var items: [Any] = [vm?.titleText ?? ""]
+        
+        if let link = vm?.photo.thumbnailUrl, let url = URL(string: link) {
+            items.append(url)
+        }
+        let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        present(ac, animated: true)
+        
     }
     
 }
